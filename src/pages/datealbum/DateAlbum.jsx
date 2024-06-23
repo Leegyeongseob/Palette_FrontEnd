75d5ebf5677de51906c4ee2447617c571c253420
goyang1103 <00bsj@naver.com>
2024-06-23 15:51:00 +0900

    커플 앨범 팝업 구현중

--- src/pages/datealbum/DateAlbum.jsx
+++ src/pages/datealbum/DateAlbum.jsx
@@ -248,12 +248,142 @@ const PlusButton = styled.button`
   }
 `;
 
+const Popup = styled.div`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 600px;
+  height: 300px;
+  background: white;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  z-index: 1000;
+`;
+
+const PopupOverlay = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 999;
+  background: rgba(0, 0, 0, 0.1); /* 투명도를 낮춤 */
+  backdrop-filter: blur(3px); /* 블러 효과 추가 */
+`;
+const PopTitle = styled.div`
+  width: 90%;
+  height: 20%;
+  display: flex;
+  flex-direction: row;
+  border-bottom: 1px solid #c8c8c8;
+  align-items: center;
+  justify-content: flex-start;
+`
+const PopBoard = styled.div`
+  width: 90%;
+  height: 60%;
+  margin-top: 0.5rem;
+  padding: 0.5rem;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  border-radius: 0.5rem;
+  background-color: white;
+`
+const BuyTema = styled.div`
+  width: 33%;
+  height: 80%;
+  font-size: 1rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  border-right: 1px solid darkgray;
+  &:last-child {
+    border-right: none;
+  }
+`
+const TemaPrice = styled.div`
+  width: 100%;
+  height: 50%;
+  font-size: 1rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+const TemaInfo = styled.div`
+width: 100%;
+height: 100%;
+  font-size: 1rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
+const TemaOne = styled.div`
+  width: 10vw;
+  height: 25vh;
+  font-size: 1.3rem;
+  margin-bottom: 1rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+const TemaTwo = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+const TemaThr = styled.div`
+  display: flex;
+  white-space: nowrap;
+  align-items: center;
+  justify-content: center;
+`
+const Strikethrough = styled.div`
+  text-decoration: line-through;
+`;
+  
+
+const CloseButton = styled.div`
+  padding: 0.5rem 1rem;
+  font-size: 0.8rem;
+  margin-top: 2px;
+  border: none;
+  border-radius: 0.5rem;
+  background-color: darkgray;
+  cursor: pointer;
+  &:hover {
+    background-color: gray;
+  }
+`
+const BuyButton = styled.div`
+  padding: 0.5rem 1rem;
+  font-size: 0.6rem;
+  margin-top: 1rem;
+  border: none;
+  border-radius: 0.5rem;
+  background-color: darkgray;
+  cursor: pointer;
+  &:hover {
+    background-color: gray;
+  }
+`
+
 
 const DateAlbum = () => {
   const [animate, setAnimate] = useState(false);
   const [imgBoxes, setImgBoxes] = useState(Array(15).fill(null).map((_, index) => (index === 0 ? '+' : null)));
   const [images, setImages] = useState(Array(15).fill(null));
   const navigate = useNavigate();
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
+  const [isTemaPopup, setIsTemaPopup] = useState(false);
+  const [isPagePopup, setIsPagePopup] = useState(false);
 
   const handleNext = () => {
     setAnimate(true);
@@ -298,6 +428,22 @@ const DateAlbum = () => {
     setImages(newImages);
   };
 
+  const handleOpenPopup = () => {
+    setIsPopupOpen(true);
+  };
+  const handleTemaPopup = () => {
+    setIsTemaPopup(true)
+  };
+  const handlePagePopup = () => {
+    setIsPagePopup(true)
+  };
+
+  const handleClosePopup = () => {
+    setIsPopupOpen(false);
+    setIsTemaPopup(false)
+    setIsPagePopup(false)
+  };
+
   return (
     <>
       <InputDetailDiv />
@@ -307,7 +453,7 @@ const DateAlbum = () => {
             <CoupleImg />
           </CoupleDiv>
           <AlbumTitle>알콩 달콩이의 앨범</AlbumTitle>
-          <AddPic>사진 업로드</AddPic>
+          <AddPic onClick={handleOpenPopup}>사진 업로드</AddPic>
           <ImgWrapper>
           {imgBoxes.slice(0, 6).map((box, index) => (
             <ImgBox
@@ -328,8 +474,8 @@ const DateAlbum = () => {
         <BookSign2 animate={animate}>
           <ContentWrapper animate={animate}>
             <AddButton>
-              <AddTema>테마 추가</AddTema>
-              <AddAlbum>앨범 추가</AddAlbum>
+              <AddTema onClick={handleTemaPopup}>테마 추가</AddTema>
+              <AddAlbum onClick={handlePagePopup}>앨범 추가</AddAlbum>
             </AddButton>
             <ImgWrapper2>
             <Dday>♥ D + 150 ♥</Dday>
@@ -352,6 +498,67 @@ const DateAlbum = () => {
       <InputDetailDiv>
         <NextButton onClick={handleNext}>▶▶</NextButton>
       </InputDetailDiv>
+      {isPopupOpen && (
+        <>
+          <PopupOverlay onClick={handleClosePopup} />
+          <Popup>
+            <PopTitle>사진 업로드</PopTitle>
+            <PopBoard></PopBoard>
+            <CloseButton onClick={handleClosePopup}>닫기</CloseButton>
+          </Popup>
+          </>
+      )}
+      {isTemaPopup && (
+        <>
+          <PopupOverlay onClick={handleClosePopup} />
+          <Popup>
+            <PopTitle>테마 구매</PopTitle>
+            <PopBoard>
+              <BuyTema>
+              <TemaPrice>
+                <TemaInfo>
+                  <TemaOne>SkyBlue Tema</TemaOne>
+                  <TemaTwo>파격세일!!</TemaTwo>
+                  <TemaThr><Strikethrough>9900원</Strikethrough>={">"}300원</TemaThr>
+                  <BuyButton>구매</BuyButton>
+                </TemaInfo>
+              </TemaPrice>
+              </BuyTema>
+              <BuyTema>
+              <TemaPrice>
+                <TemaInfo>
+                  <TemaOne>Black Tema</TemaOne>
+                  <TemaTwo>파격세일!!</TemaTwo>
+                  <TemaThr><Strikethrough>59900원</Strikethrough>={">"}500원</TemaThr>
+                  <BuyButton>구매</BuyButton>
+                </TemaInfo>
+              </TemaPrice>
+              </BuyTema>
+              <BuyTema>
+              <TemaPrice>
+                <TemaInfo>
+                  <TemaOne>Pink Tema</TemaOne>
+                  <TemaTwo>파격세일!!</TemaTwo>
+                  <TemaThr><Strikethrough>129800원</Strikethrough>={">"}900원</TemaThr>
+                  <BuyButton>구매</BuyButton>
+                </TemaInfo>
+              </TemaPrice>
+              </BuyTema>
+            </PopBoard>
+            <CloseButton onClick={handleClosePopup}>닫기</CloseButton>
+          </Popup>
+          </>
+      )}
+      {isPagePopup && (
+        <>
+          <PopupOverlay onClick={handleClosePopup} />
+          <Popup>
+            <PopTitle>페이지 구매</PopTitle>
+            <PopBoard></PopBoard>
+            <CloseButton onClick={handleClosePopup}>닫기</CloseButton>
+          </Popup>
+          </>
+      )}
     </>
   );
 };
