7c2785c28798e6230a8350d7b1a1aa2e46b6641a
goyang1103 <00bsj@naver.com>
2024-07-03 23:32:53 +0900

    광고 문의 페이지 구성

--- src/pages/palette/PaletteAd.jsx
+++ src/pages/palette/PaletteAd.jsx
@@ -12,18 +12,9 @@ const Background = styled.div`
   flex-direction: column;
   font-size: 1.8rem;
 `;
-const Container = styled.div`
-  width: 100%;
-  height: 90%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background-color: #fff9f2;
-  position: sticky;
-`;
 
-const BoardWrapper = styled.div`
-  width: 70%;
+const AdWrapper = styled.div`
+  width: 100%;
   height: 90%;
   display: flex;
   flex-direction: row;
@@ -41,49 +32,6 @@ const Board = styled.div`
   background-color: #fff9f0;
 `;
 
-const HelpRoot = styled.div`
-  width: 90%;
-  height: 5%;
-  display: flex;
-  flex-direction: row;
-  font-size: 0.8vw;
-  justify-content: flex-start;
-  align-items: center;
-`;
-
-const Root = styled(Link)`
-  width: 6%;
-  height: 100%;
-  display: flex;
-  text-decoration: none;
-  color: #000;
-  &:hover {
-    font-weight: bolder;
-  }
-`;
-const Root2 = styled(Root)`
-  width: 2%;
-`;
-const Root3 = styled(Root)`
-  width: 6.5%;
-`;
-const Root4 = styled(Root)`
-  width: 10%;
-`;
-
-const SearchBox = styled.div`
-  width: 90%;
-  height: 15%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-const HelpBoard = styled.div`
-  width: 90%;
-  height: 70%;
-  display: flex;
-  flex-direction: column;
-`;
 
 const AdPage = () => {
   return (
@@ -91,22 +39,8 @@ const AdPage = () => {
       <Globalstyle />
       <Header />
       <Background>
-        <Container>
-          <BoardWrapper>
-            <Category />
-            <Board>
-              <HelpRoot>
-                <Root to="/">Palette</Root>
-                <Root2>{">"}</Root2>
-                <Root3 to="/customer">고객센터</Root3>
-                <Root2>{">"}</Root2>
-                <Root4 to="/customer/ad">광고 문의</Root4>
-              </HelpRoot>
-              <SearchBox></SearchBox>
-              <HelpBoard></HelpBoard>
-            </Board>
-          </BoardWrapper>
-        </Container>
+          <AdWrapper>
+          </AdWrapper>
         <Footer />
       </Background>
     </>
