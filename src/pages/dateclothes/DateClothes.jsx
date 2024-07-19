8bfbcafe084ef4c8062a7af83fadd2a70f6f8a87
user1 <98tmddyddl@gmail.com>
2024-07-19 14:10:13 +0900

    데이트룩css

--- src/pages/dateclothes/DateClothes.jsx
+++ src/pages/dateclothes/DateClothes.jsx
@@ -3,6 +3,7 @@ import clothesBg from "../../img/album/1.jpeg";
 import CoupleImage from "../../common/couple/CoupleImg";
 import Swiper from "./Swiper";
 import { useState } from "react";
+import clothesBg1 from "../../img/background/theme/clothes_background.jpg";
 const BookSign = styled.div`
   width: 100%;
   aspect-ratio: 50/20;
@@ -11,22 +12,24 @@ const BookSign = styled.div`
   justify-content: end;
   flex-direction: column;
   align-items: center;
-
-  & > .save1 {
-    justify-content: first baseline;
-  }
-  & > .save2 {
-    justify-content: end;
-  }
-  & > .coupleimg {
-    margin-left: 104%;
-    aspect-ratio: 203/35;
-    display: flex;
-    justify-content: center;
-    width: 203%;
-    height: auto;
-    position: relative;
-  }
+`;
+const Save2 = styled.div`
+  justify-content: end;
+`;
+const Save = styled.div`
+  justify-content: first baseline;
+`;
+const CoupleimgCon = styled.div`
+  margin-left: 102.5%;
+  /* aspect-ratio: 203/35; */
+  display: flex;
+  justify-content: center;
+  width: 102.5%;
+  height: auto;
+  background-color: black;
+  position: relative;
+  margin-bottom: 5vh;
+  gap: 500px;
 `;
 const BookSign2 = styled.div`
   width: 100%;
@@ -36,9 +39,7 @@ const BookSign2 = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: end;
-  & > .save1 {
-    justify-content: first baseline;
-  }
+
   & > .save2 {
     justify-content: end;
   }
@@ -57,12 +58,22 @@ const BookTheme = styled.div`
   height: 81.5%;
   margin-top: 5vh;
   margin-left: 0.7vw;
-  background-image: url(${clothesBg});
+  /* background-image: url(${clothesBg1}); */
   background-size: cover;
   background-position: left;
   background-repeat: no-repeat;
   display: flex;
   justify-content: space-between;
+  @media screen and (max-width: 1200px) {
+    width: 41.5%;
+    height: 56vh;
+    margin-top: 4vh;
+  }
+  @media screen and (max-width: 768px) {
+    width: 41.5%;
+    height: 34.5vh;
+    margin-top: 3vh;
+  }
 `;
 
 const BookTheme2 = styled.div`
@@ -70,30 +81,53 @@ const BookTheme2 = styled.div`
   height: 81.5%;
   margin-top: 5vh;
   margin-left: 0.1vw;
-  background-image: url(${clothesBg});
+  /* background-image: url(${clothesBg1}); */
   background-size: cover;
   background-position: right;
   background-repeat: no-repeat;
   display: flex;
   justify-content: space-between;
+  @media screen and (max-width: 1200px) {
+    width: 41.5%;
+    height: 56vh;
+    margin-top: 4vh;
+  }
+  @media screen and (max-width: 768px) {
+    width: 41.5%;
+    height: 34.5vh;
+    margin-top: 3vh;
+  }
 `;
 const Title = styled.div`
-  width: 500px;
+  width: 100%;
   height: 6vh;
   font-family: "Courier New", Courier, monospace;
   font-size: 28px;
   font-weight: 600;
   display: flex;
   align-items: center;
-  padding-top: 3vh;
-  padding-left: 2vw;
+  /* padding-top: 3vh; */
+  margin-top: 10vh;
+  position: relative;
+  /* padding-left: 2vw; */
+  @media screen and (max-width: 1200px) {
+    position: relative;
+    top: 10vh;
+  }
+  @media screen and (max-width: 768px) {
+    width: 41.5%;
+    height: 34.5vh;
+    margin-top: 3vh;
+  }
 `;
+
 const BookDiv = styled.div`
   width: auto;
   height: auto;
   display: flex;
   justify-content: space-between;
 `;
+
 const Options = styled.div`
   width: 100px;
   height: 4vh;
@@ -112,18 +146,20 @@ const Options = styled.div`
   }
 `;
 const OptionsSelectDiv = styled.div`
-  width: auto;
-  height: auto;
+  /* width: auto;
+  height: auto; */
+  height: 4vh;
+  /* background-color: aqua; */
   display: flex;
   z-index: 100;
-  align-items: end;
+  /* align-items: end; */
 `;
 
 const ButtonDiv = styled.div`
-  width: 23.438vw;
-  height: 9.962vh;
   display: flex;
-  align-items: center;
+  /* background-color: #49cf8c; */
+  justify-content: center;
+  align-items: end;
 `;
 const StroeButton = styled.div`
   width: 5vw;
@@ -136,20 +172,60 @@ const StroeButton = styled.div`
   justify-content: center;
   align-items: center;
   font-weight: 500;
+
   cursor: pointer;
   &:hover {
     background-color: rgba(0, 0, 0, 0.6);
   }
 `;
+const ClothesFormContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  align-items: end;
+`;
+
 const ClothesForm = styled.div`
   width: 253px;
   height: auto;
   background-color: #fff;
+  margin-right: 15%;
   border-radius: 0.521vw;
   border: 2px solid pink;
   display: flex;
   flex-direction: column;
   align-items: center;
+  @media screen and (max-width: 1200px) {
+    width: 65%;
+    height: 35vh;
+    margin-top: 4vh;
+  }
+  @media screen and (max-width: 768px) {
+    width: 41.5%;
+    height: 20.5vh;
+    margin-top: 3vh;
+  }
+`;
+const ClothesForm2 = styled.div`
+  width: 253px;
+  height: auto;
+  background-color: #fff;
+  margin-left: 15%;
+  border-radius: 0.521vw;
+  border: 2px solid pink;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  @media screen and (max-width: 1200px) {
+    width: 65%;
+    height: 35vh;
+    margin-top: 4vh;
+  }
+  @media screen and (max-width: 768px) {
+    width: 41.5%;
+    height: 20.5vh;
+    margin-top: 3vh;
+  }
 `;
 const DateClothes = () => {
   const [isOnePiece, setIsOnePiece] = useState(false);
@@ -158,51 +234,59 @@ const DateClothes = () => {
       <BookTheme>
         <BookSign>
           <Title>데이트룩 코디</Title>
-          <div className="coupleimg">
-            <CoupleImage clothes={true} />
-          </div>
-          <BookDiv />
-
-          <ClothesForm>
-            <Swiper clothNum={1} />
-            <Swiper clothNum={2} />
-            <Swiper shoes={true} clothNum={3} />
-          </ClothesForm>
-          <ButtonDiv className="save1">
-            <StroeButton>저장</StroeButton>
-          </ButtonDiv>
+          <CoupleimgCon>
+            {/* <CoupleImage clothes={true} /> */}
+            <CoupleImage />
+          </CoupleimgCon>
+          {/* <BookDiv /> */}
+          <ClothesFormContainer>
+            <ButtonDiv>
+              {/* <Save> */}
+              <StroeButton>저장</StroeButton>
+              {/* </Save> */}
+            </ButtonDiv>
+            <ClothesForm>
+              <Swiper clothNum={1} />
+              <Swiper clothNum={2} />
+              <Swiper shoes={true} clothNum={3} />
+            </ClothesForm>
+          </ClothesFormContainer>
         </BookSign>
       </BookTheme>
       <BookTheme2>
+        <div className="clothDiv" />
+        {/* <div className="optionSDiv"> */}
+
+        {/* </div> */}
         <BookSign2>
-          <div className="clothDiv" />
-          <div className="optionSDiv">
-            <OptionsSelectDiv>
-              <Options
-                onClick={() => {
-                  setIsOnePiece(true);
-                }}
-              >
-                원피스
-              </Options>
-              <Options
-                onClick={() => {
-                  setIsOnePiece(false);
-                }}
-              >
-                상＆하의
-              </Options>
-            </OptionsSelectDiv>
-          </div>
-          <ClothesForm>
-            <Swiper clothNum={4} OnePiece={isOnePiece} />
-            <Swiper clothNum={5} OnePiece={isOnePiece} />
-            <Swiper clothNum={7} OnePiece={isOnePiece} />
-            <Swiper shoes={true} clothNum={6} />
-          </ClothesForm>
-          <ButtonDiv className="save2">
-            <StroeButton>저장</StroeButton>
-          </ButtonDiv>
+          <OptionsSelectDiv>
+            <Options
+              onClick={() => {
+                setIsOnePiece(true);
+              }}
+            >
+              원피스
+            </Options>
+            <Options
+              onClick={() => {
+                setIsOnePiece(false);
+              }}
+            >
+              상＆하의
+            </Options>
+          </OptionsSelectDiv>
+
+          <ClothesFormContainer>
+            <ClothesForm2>
+              <Swiper clothNum={4} OnePiece={isOnePiece} />
+              <Swiper clothNum={5} OnePiece={isOnePiece} />
+              <Swiper clothNum={7} OnePiece={isOnePiece} />
+              <Swiper shoes={true} clothNum={6} />
+            </ClothesForm2>
+            <ButtonDiv>
+              <StroeButton>저장</StroeButton>
+            </ButtonDiv>
+          </ClothesFormContainer>
         </BookSign2>
       </BookTheme2>
     </>
